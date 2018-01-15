70aa0927139054e67fc150bd0377a865659739bb
crosdam <35403651+crosdam@users.noreply.github.com>
2018-01-15 17:48:33 +0100

    Add files via upload

---
+++ cros-a-p00-HelloWorld/src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld
+{
+	public static void main(String[] args)
+  {
+// Instrucción de sistema, salida por la pantalla, y que imprima
+		System.out.println("Hola Mundo");
+	}
+}
