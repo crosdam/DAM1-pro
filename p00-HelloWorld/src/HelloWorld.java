5bfb6eac9decfe3b6703ab0a40172960fd5b23e5
Carlos Ros <hello@carlosros.com>
2018-01-15 18:00:56 +0100

    Changes

--- p00-HelloWorld/src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld
-{
-	public static void main(String[] args)
-  {
-// Instrucción de sistema, salida por la pantalla, y que imprima
-		System.out.println("Hola Mundo");
-	}
-}
